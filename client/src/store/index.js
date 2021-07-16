1b64d592425dbd3e4c15a310cceb9b852a00b024
Nacho Hotz <juanhotz611@gmail.com>
2021-07-16 12:42:52 -0300

    Done redux store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from '../reducers/index';
+import rootReducer from '../reducer/index';
 
 const composeEnhancers = typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 const store = createStore(
