0e6cf52ae79ffccb4c2aef90a6872d5a1d4a7182
Nacho Hotz <juanhotz611@gmail.com>
2021-07-16 12:41:40 -0300

    Finished landing page

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Home from '../Home/Home';
 import Style from './Landing.module.css';
+import Home from '../Home/Home';
 
 export default function Landing() {
   return (
-    <div>
-      <Link to={Home} className={Style.button}>Go!</Link>
-    </div>
+    <main>
+      <Link to={Home} className={Style.link}>Enter</Link>
+    </main>
   );
 }
