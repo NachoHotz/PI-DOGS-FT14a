bbdb80d68406e1ed074fd5fb37616d8a7951c91e
Nacho Hotz <juanhotz611@gmail.com>
2021-07-16 12:43:24 -0300

    Done defining routes

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Landing from '../components/Landing/Landing';
-import Home from '../components/Home/Home';
 import Nav from '../components/Nav/Nav';
+import Searchbar from '../components/SearchBar/SearchBar';
+import Home from '../components/Home/Home';
 import BreedDetail from '../components/BreedDetail/BreedDetail';
 import BreedCreation from '../components/BreedCreation/BreedCreation';
 
@@ -10,10 +11,11 @@ function App() {
   return (
     <div>
       <Route exact path="/" component={Landing} />
-      <Nav />
-      <Route path="/home" component={Home} />
+      <Route exact path="/:any" component={Nav} />
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/home" component={Searchbar} />
       <Route path="/detail/:id" component={BreedDetail} />
-      <Route paath="/creation" component={BreedCreation} />
+      <Route exact paath="/creation" component={BreedCreation} />
     </div>
   );
 }
