763aecb205fd83134faa46a0ff6029d55cb52d7f
Nacho Hotz <juanhotz611@gmail.com>
2021-07-16 12:42:22 -0300

    Initial reducer work

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,15 +1,15 @@
-import {GET_BREEDS} from "../action/names";
+import GET_BREEDS from '../action/names';
 
 const initialState = {
   allBreeds: [],
   breedDetail: {},
 };
 
-export default function rootReducer(state = initialState, action) {
+const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_BREEDS: {
-
-    }
-    default: return state,
+    case GET_BREEDS: return { ...state, allBreeds: action.payload };
+    default: return state;
   }
-}
+};
+
+export default rootReducer;
