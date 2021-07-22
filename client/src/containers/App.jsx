8452195633601980d66742f5aab16ed9b64a19a2
Nacho Hotz <juanhotz611@gmail.com>
2021-07-22 18:04:19 -0300

    Done App.js

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Landing from '../components/Landing/Landing';
-import Nav from '../components/Nav/Nav';
-import Searchbar from '../components/SearchBar/SearchBar';
 import Home from '../components/Home/Home';
 import BreedDetail from '../components/BreedDetail/BreedDetail';
 import BreedCreation from '../components/BreedCreation/BreedCreation';
+import './App.css';
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={Landing} />
-      <Route exact path="/:any" component={Nav} />
       <Route exact path="/home" component={Home} />
-      <Route exact path="/home" component={Searchbar} />
-      <Route path="/detail/:id" component={BreedDetail} />
-      <Route exact paath="/creation" component={BreedCreation} />
+      <Switch>
+        <Route exact path="/home/creation" component={BreedCreation} />
+        <Route exact path="/home/:id" render={({ match }) => <BreedDetail id={match.params.id} />} />
+      </Switch>
     </div>
   );
 }
