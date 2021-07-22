70c8a0f27cc7fef6869301908f2621fccab1cb71
Nacho Hotz <juanhotz611@gmail.com>
2021-07-22 18:06:03 -0300

    Done reducer

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,13 +1,47 @@
-import GET_BREEDS from '../action/names';
+/* eslint-disable max-len */
+/* eslint-disable no-case-declarations */
+/* eslint-disable arrow-parens */
+import {
+  GET_ALL_BREEDS,
+  GET_BREED_DETAIL,
+  GET_BREEDS_NAME,
+  CREATE_BREED,
+  GET_TEMPERAMENTS,
+  GET_BREEDS_TEMP,
+  SORT_NAME_ASC,
+  SORT_NAME_DESC,
+  SORT_WEIGHT_ASC,
+  SORT_WEIGHT_DESC,
+} from '../action/names';
 
 const initialState = {
   allBreeds: [],
+  breedsSearch: [],
   breedDetail: {},
+  temperaments: [],
 };
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_BREEDS: return { ...state, allBreeds: action.payload };
+    case GET_ALL_BREEDS: return { ...state, allBreeds: action.payload };
+    case GET_BREEDS_NAME: return { ...state, breedsSearch: action.payload };
+    case GET_BREED_DETAIL: return { ...state, breedDetail: action.payload };
+    case GET_TEMPERAMENTS: return { ...state, temperaments: action.payload };
+    case CREATE_BREED: return { ...state, allBreeds: action.payload };
+    case 'all': return { ...state, breedsSearch: state.allBreeds };
+    case GET_BREEDS_TEMP: return { ...state, breedsSearch: action.payload };
+    case 'created': return {
+      ...state,
+      breedsSearch: state.allBreeds.filter(breed => breed.id.length > 3).sort(),
+    };
+    case 'notcreated': return {
+      ...state,
+      breedsSearch: state.allBreeds.filter(breed => breed.id < 300).sort(),
+    };
+    case SORT_NAME_ASC: return { ...state, allBreeds: action.payload };
+    case SORT_NAME_DESC: return { ...state, allBreeds: action.payload };
+    case SORT_WEIGHT_ASC: return { ...state, allBreeds: action.payload };
+    case SORT_WEIGHT_DESC: return { ...state, allBreeds: action.payload };
     default: return state;
   }
 };
