3671096b5c764e91a9e87af11228c4be717adab6
Nacho Hotz <juanhotz611@gmail.com>
2021-07-22 18:03:16 -0300

    Done navbar

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Style from './Nav.module.css';
-import { BreedCreation } from '../BreedCreation/BreedCreation';
 
 export default function Nav() {
   return (
     <header>
-      <nav>
-        <Link to={BreedCreation} className={Style.createButton}>Create Breed</Link>
+      <nav className={Style.navBar}>
+        <Link to="/home/creation" className={Style.create}>Create Breed</Link>
       </nav>
     </header>
   );
