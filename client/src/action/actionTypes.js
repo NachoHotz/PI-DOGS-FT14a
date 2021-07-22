b5fdf980c050b78331224fda4c67c82f99a5f704
Nacho Hotz <juanhotz611@gmail.com>
2021-07-22 17:58:19 -0300

    Done actions

--- client/src/action/actionTypes.js
+++ client/src/action/actionTypes.js
@@ -1,19 +1,74 @@
 /* eslint-disable func-names */
+/* eslint-disable consistent-return */
+/* eslint-disable no-console */
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable radix */
 import axios from 'axios';
 import {
   GET_ALL_BREEDS,
   GET_BREED_DETAIL,
-  NEXT_PAGE,
-  PREVIOUS_PAGE,
+  GET_BREEDS_CREATOR,
+  GET_TEMPERAMENTS,
+  CREATE_BREED,
+  SORT_NAME_ASC,
+  SORT_NAME_DESC,
+  GET_BREEDS_NAME,
+  GET_BREEDS_TEMP,
+  SORT_WEIGHT_ASC,
+  SORT_WEIGHT_DESC,
 } from './names';
 
 const SERVER = 'http://localhost:3001';
 
-export function getBreeds(name, sort, creator) {
+export function getBreeds() {
   return async function (dispatch) {
     try {
-      const breeds = await axios.get(`${SERVER}/dogs?name=${name}&creator=${creator}&sort=${sort}`);
-      dispatch({ type: GET_ALL_BREEDS, payload: breeds.data });
+      const breeds = await axios.get(`${SERVER}/dogs`);
+      return dispatch({ type: GET_ALL_BREEDS, payload: breeds.data });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+}
+
+export function getBreedsByName(name) {
+  return async function (dispatch) {
+    try {
+      const breeds = await axios.get(`${SERVER}/dogs?name=${name}`);
+      return dispatch({ type: GET_BREEDS_NAME, payload: breeds.data });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+}
+
+export function getBreedsByTemp(filtered) {
+  return function (dispatch) {
+    return dispatch({ type: GET_BREEDS_TEMP, payload: filtered });
+  };
+}
+
+export function getBreedsCreator(creator) {
+  return function (dispatch) {
+    if (creator === 'all') {
+      console.log('entre');
+      return dispatch({ type: 'all' });
+    }
+    if (creator === 'created') {
+      return dispatch({ type: 'created' });
+    }
+    if (creator === 'notcreated') {
+      return dispatch({ type: 'notcreated' });
+    }
+  };
+}
+
+export function getTemperaments() {
+  return async function (dispatch) {
+    try {
+      const temperaments = await axios.get(`${SERVER}/temperaments`);
+      return dispatch({ type: GET_TEMPERAMENTS, payload: temperaments.data });
     } catch (e) {
       console.log(e);
     }
@@ -22,11 +77,84 @@ export function getBreeds(name, sort, creator) {
 
 export function getBreedDetailId(id) {
   return async function (dispatch) {
+    const detail = await axios.get(`${SERVER}/dogs/${id}`);
+    return dispatch({ type: GET_BREED_DETAIL, payload: detail.data });
+  };
+}
+
+export function createBreed(breedForm) {
+  return async function (dispatch) {
     try {
-      const breedDetail = await axios.get(`${SERVER}/dogs/${id}`);
-      dispatch({ type: GET_BREED_DETAIL, payload: breedDetail.data });
+      const createdBreed = await axios.post(`${SERVER}/dogs`, {
+        name: breedForm.name,
+        height: breedForm.height,
+        weight: breedForm.weight,
+        life_span: breedForm.life_span,
+        image: breedForm.image,
+        temperament: breedForm.temperament,
+      });
+      return dispatch({ type: CREATE_BREED, payload: createdBreed.data });
     } catch (e) {
       console.log(e);
+    }
+  };
+}
+
+export function Sort(method) {
+  return async function (dispatch) {
+    if (method === 'A-Z') {
+      let breeds = await axios.get(`${SERVER}/dogs`);
+      breeds = breeds.data;
+      const breedsSorted = breeds.sort((a, b) => {
+        if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+      });
+      return dispatch({ type: SORT_NAME_ASC, payload: breedsSorted });
+    }
+    if (method === 'Z-A') {
+      let breeds = await axios.get(`${SERVER}/dogs`);
+      breeds = breeds.data;
+      const breedsSorted = breeds.sort((a, b) => {
+        if (a.name.toLowerCase() < b.name.toLowerCase()) return 1;
+        if (a.name.toLowerCase() > b.name.toLowerCase()) return -1;
+        return 0;
+      });
+      return dispatch({ type: SORT_NAME_DESC, payload: breedsSorted });
+    }
+    if (method === 'Light') {
+      let breeds = await axios.get(`${SERVER}/dogs`);
+      breeds = breeds.data;
+      const breedsSorted = breeds.sort((a, b) => {
+        if (!a.weight.metric) {
+          const metric = a.weight;
+          a.weight = metric;
+        }
+
+        if (!b.weight.metric) {
+          const metric = { metric: b.weight };
+          b.weight = metric;
+        }
+        if (parseInt(a.weight.metric) > parseInt(b.weight.metric)) return 1;
+        if (parseInt(a.weight.metric) < parseInt(b.weight.metric)) return -1;
+        return 0;
+      });
+      return dispatch({ type: SORT_WEIGHT_ASC, payload: breedsSorted });
+    }
+
+    if (method === 'Heavy') {
+      let breeds = await axios.get(`${SERVER}/dogs`);
+      breeds = breeds.data;
+      const breedsSorted = breeds.sort((a, b) => {
+        if (b.id.length > 3) {
+          const metric = { metric: b.weight };
+          b.weight = metric;
+        }
+        if (parseInt(b.weight.metric) < parseInt(a.weight.metric)) return 1;
+        if (parseInt(b.weight.metric) > parseInt(a.weight.metric)) return -1;
+        return 0;
+      });
+      return dispatch({ type: SORT_WEIGHT_DESC, payload: breedsSorted });
     }
   };
 }
