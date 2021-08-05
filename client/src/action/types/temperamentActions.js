bb993d42bf8da9982d62ac683b1e60ea585d8e55
Nacho Hotz <juanhotz611@gmail.com>
2021-08-05 13:40:24 -0300

    delete(client/src/actions/types): eslint rules

--- client/src/action/types/temperamentActions.js
+++ client/src/action/types/temperamentActions.js
@@ -1,9 +1,5 @@
 /* eslint-disable func-names */
-/* eslint-disable consistent-return */
 /* eslint-disable no-console */
-/* eslint-disable guard-for-in */
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable radix */
 import axios from 'axios';
 import { GET_TEMPERAMENTS } from '../names';
 import { TEMPERAMENTS_ENDPOINT } from '../../constants';
