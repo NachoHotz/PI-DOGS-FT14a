98c03ba9fd308ac68dbf7d6de190e3ab5ac77cb4
Nacho Hotz <juanhotz611@gmail.com>
2021-08-05 13:35:51 -0300

    refactor(client/src/actions): sort

---
+++ client/src/action/types/sort.js
@@ -0,0 +1,66 @@
+/* eslint-disable func-names */
+/* eslint-disable consistent-return */
+/* eslint-disable no-console */
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable radix */
+import axios from 'axios';
+
+import {
+  SORT_NAME_ASC,
+  SORT_NAME_DESC,
+  SORT_WEIGHT_ASC,
+  SORT_WEIGHT_DESC,
+} from '../names';
+
+import { BREEDS_ENDPOINT } from '../../constants';
+
+export default function Sort(method) {
+  return async function (dispatch) {
+    if (method === 'A-Z') {
+      let breeds = await axios.get(`${BREEDS_ENDPOINT}`);
+      breeds = breeds.data;
+      const breedsSorted = breeds.sort((a, b) => {
+        if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+      });
+      return dispatch({ type: SORT_NAME_ASC, payload: breedsSorted });
+    }
+
+    if (method === 'Z-A') {
+      let breeds = await axios.get(`${BREEDS_ENDPOINT}`);
+      breeds = breeds.data;
+      const breedsSorted = breeds.sort((a, b) => {
+        if (a.name.toLowerCase() < b.name.toLowerCase()) return 1;
+        if (a.name.toLowerCase() > b.name.toLowerCase()) return -1;
+        return 0;
+      });
+      return dispatch({ type: SORT_NAME_DESC, payload: breedsSorted });
+    }
+
+    if (method === 'Light') {
+      let breeds = await axios.get(`${BREEDS_ENDPOINT}`);
+      breeds = breeds.data;
+
+      const breedsSorted = breeds.sort((a, b) => {
+        if (parseInt(a.weight.metric) > parseInt(b.weight.metric)) return 1;
+        if (parseInt(a.weight.metric) < parseInt(b.weight.metric)) return -1;
+        return 0;
+      });
+      return dispatch({ type: SORT_WEIGHT_ASC, payload: breedsSorted });
+    }
+
+    if (method === 'Heavy') {
+      let breeds = await axios.get(`${BREEDS_ENDPOINT}`);
+      breeds = breeds.data;
+
+      const breedsSorted = breeds.sort((a, b) => {
+        if (parseInt(a.weight.metric) < parseInt(b.weight.metric)) return 1;
+        if (parseInt(a.weight.metric) > parseInt(b.weight.metric)) return -1;
+        return 0;
+      });
+      return dispatch({ type: SORT_WEIGHT_DESC, payload: breedsSorted });
+    }
+  };
+}
