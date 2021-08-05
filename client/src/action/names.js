b3a28eb3169891d36b11ecdf85acecb52db7b041
Nacho Hotz <juanhotz611@gmail.com>
2021-08-05 17:26:20 -0300

    feat(client/src/actions): add comments

--- client/src/action/names.js
+++ client/src/action/names.js
@@ -1,9 +1,14 @@
+// dogs names
 export const GET_ALL_BREEDS = 'GET_BREEDS';
 export const GET_BREEDS_CREATOR = 'GET_BREEDS_CREATOR';
 export const GET_BREEDS_NAME = 'GET_BREEDS_NAME';
 export const GET_BREEDS_TEMP = 'GET_BREEDS_TEMP';
-export const GET_TEMPERAMENTS = 'GET_TEMPERAMENTS';
 export const CREATE_BREED = 'CREATE_BREED';
+
+// temperament name
+export const GET_TEMPERAMENTS = 'GET_TEMPERAMENTS';
+
+// sort names
 export const SORT_NAME_ASC = 'SORT_NAME_ASC';
 export const SORT_NAME_DESC = 'SORT_NAME_DESC';
 export const SORT_WEIGHT_ASC = 'SORT_WEIGHT_ASC';
