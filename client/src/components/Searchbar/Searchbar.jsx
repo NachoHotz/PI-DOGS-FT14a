f2e73d535358403f31d996c0c2163ff5918eca8e
Nacho Hotz <juanhotz611@gmail.com>
2021-08-05 17:19:02 -0300

    fix(client/src/components/Searchbar): import

--- client/src/components/Searchbar/Searchbar.jsx
+++ client/src/components/Searchbar/Searchbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { getBreedsByName } from '../../action/types/dogsActions';
+import { getBreedsByName } from '../../action/types/dogActions/getBreedsByName';
 import Style from './Searchbar.module.css';
 
 export default function Searchbar() {
