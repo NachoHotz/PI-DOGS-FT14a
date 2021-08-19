3a7a4d311abeb771286acba2ed958e7028616aa3
Nacho Hotz <juanhotz611@gmail.com>
2021-08-19 14:26:40 -0300

    fix actionTypes import in reducer

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,17 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable no-case-declarations */
 /* eslint-disable arrow-parens */
-import {
-  GET_ALL_BREEDS,
-  GET_BREEDS_NAME,
-  CREATE_BREED,
-  GET_TEMPERAMENTS,
-  GET_BREEDS_TEMP,
-  SORT_NAME_ASC,
-  SORT_NAME_DESC,
-  SORT_WEIGHT_ASC,
-  SORT_WEIGHT_DESC,
-} from '../action/names';
+import * as actionTypes from '../actions/names';
 
 const initialState = {
   allBreeds: [],
@@ -21,15 +11,15 @@ const initialState = {
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_ALL_BREEDS: return { ...state, allBreeds: action.payload };
-    case GET_BREEDS_NAME: return { ...state, breedsFiltered: action.payload };
-    case GET_TEMPERAMENTS: return { ...state, temperaments: action.payload };
-    case CREATE_BREED: return { ...state, allBreeds: state.allBreeds.concat(action.payload) };
-    case GET_BREEDS_TEMP: return { ...state, breedsFiltered: action.payload };
-    case SORT_NAME_ASC: return { ...state, allBreeds: action.payload };
-    case SORT_NAME_DESC: return { ...state, allBreeds: action.payload };
-    case SORT_WEIGHT_ASC: return { ...state, allBreeds: action.payload };
-    case SORT_WEIGHT_DESC: return { ...state, allBreeds: action.payload };
+    case actionTypes.GET_ALL_BREEDS: return { ...state, allBreeds: action.payload };
+    case actionTypes.GET_BREEDS_NAME: return { ...state, breedsFiltered: action.payload };
+    case actionTypes.GET_TEMPERAMENTS: return { ...state, temperaments: action.payload };
+    case actionTypes.CREATE_BREED: return { ...state, allBreeds: state.allBreeds.concat(action.payload) };
+    case actionTypes.GET_BREEDS_TEMP: return { ...state, breedsFiltered: action.payload };
+    case actionTypes.SORT_NAME_ASC: return { ...state, allBreeds: action.payload };
+    case actionTypes.SORT_NAME_DESC: return { ...state, allBreeds: action.payload };
+    case actionTypes.SORT_WEIGHT_ASC: return { ...state, allBreeds: action.payload };
+    case actionTypes.SORT_WEIGHT_DESC: return { ...state, allBreeds: action.payload };
     case 'all': return { ...state, breedsFiltered: state.allBreeds };
     case 'created': return { ...state, breedsFiltered: state.allBreeds.filter(breed => breed.id.length > 3).sort() };
     case 'notcreated': return { ...state, breedsFiltered: state.allBreeds.filter(breed => breed.id < 300).sort() };
