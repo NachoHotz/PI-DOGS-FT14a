39481aec0f4735e02fc42884b050122007929158
Nacho Hotz <juanhotz611@gmail.com>
2021-08-21 19:22:26 -0300

    modify store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducer/index';
 
-const composeEnhancers = typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk)),
-);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
