559ae7073396f38acfc486c43d6eceab1e143d40
Nacho Hotz <juanhotz611@gmail.com>
2021-08-21 18:12:56 -0300

    add Filters, Order and Searchbar to Navbar

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Filters from '../Filters/Filter';
+import Order from '../Order/Order';
+import Searchbar from '../Searchbar/Searchbar';
 import Style from './Nav.module.css';
 
 export default function Nav() {
   return (
     <header>
-      <nav className={Style.navBar}>
+      <nav className={Style.navBar_container}>
+        <Searchbar />
+        <Filters />
+        <Order />
         <Link to="/home/creation" className={Style.create}>Create Breed</Link>
       </nav>
     </header>
