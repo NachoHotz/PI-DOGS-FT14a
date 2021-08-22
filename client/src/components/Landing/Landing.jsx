ceb6803c1a29dc28f6d35a8541b6487a830ccdf4
Nacho Hotz <juanhotz611@gmail.com>
2021-08-22 16:44:32 -0300

    add background image

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -4,11 +4,13 @@ import Style from './Landing.module.css';
 
 export default function Landing() {
   return (
-    <div>
-      <h2 className={Style.title}>The dogs wiki</h2>
-      <Link to="/home/">
-        <button className={Style.link}>Enter</button>
-      </Link>
+    <div className={Style.landingBody}>
+      <div>
+        <h2 className={Style.title}>The dogs wiki</h2>
+        <Link to="/home/">
+          <button className={Style.link}>Enter</button>
+        </Link>
+      </div>
     </div>
   );
 }
