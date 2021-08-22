bbd198748c2309d6886d541f6a83a061476b52d6
Nacho Hotz <juanhotz611@gmail.com>
2021-08-22 15:12:54 -0300

    modify Sort action

--- client/src/redux/actions/types/dogActions/getBreedsByTemp.js
+++ client/src/redux/actions/types/dogActions/getBreedsByTemp.js
@@ -1,15 +1,8 @@
 /* eslint-disable func-names */
 /* eslint-disable no-console */
 /* eslint-disable import/prefer-default-export */
-import axios from 'axios';
 import * as actionTypes from '../../names';
-import * as endpoints from '../../../../utils/endpoints';
 
 export function getBreedsByTemp(temp) {
-  return async function (dispatch) {
-    const { data } = await axios.get(`${endpoints.BREEDS_ENDPOINT}`);
-    const filtered = [];
-
-    return dispatch({ type: actionTypes.GET_BREEDS_TEMP, payload: filtered });
-  };
+  return { type: actionTypes.GET_BREEDS_TEMP, payload: temp };
 }
--- client/src/redux/actions/types/dogActions/getBreedsCreator.js
+++ client/src/redux/actions/types/dogActions/getBreedsCreator.js
@@ -1,7 +1,15 @@
 /* eslint-disable import/prefer-default-export */
+/*eslint-disable func-names*/
+import axios from 'axios';
+import * as endpoints from '../../../../utils/endpoints';
 
 export function getBreedsCreator(creator) {
-  if (creator === 'all') return { type: 'all' };
-  if (creator === 'created') return { type: 'created' };
-  if (creator === 'notcreated') return { type: 'notcreated' };
+  return async function (dispatch) {
+    if (creator === 'all') {
+      const { data } = await axios.get(`${endpoints.BREEDS_ENDPOINT}`);
+      return dispatch({ type: 'all', payload: data });
+    }
+    if (creator === 'created') return dispatch({ type: 'created' });
+    if (creator === 'notcreated') return dispatch({ type: 'notcreated' });
+  };
 }
