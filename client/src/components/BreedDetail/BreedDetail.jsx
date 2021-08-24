4cc1dc946eeb1066c0cfe40842c4f15888e20dcb
Nacho Hotz <juanhotz611@gmail.com>
2021-08-24 20:47:48 -0300

    fix linter in ProductDetail

--- client/src/components/BreedDetail/BreedDetail.jsx
+++ client/src/components/BreedDetail/BreedDetail.jsx
@@ -5,18 +5,22 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getBreeds } from '../../redux/actions/types/breedActions';
 import Style from './BreedDetail.module.css';
 
-{ /*eslint-disable no-return-assign*/ }
+{
+  /*eslint-disable no-return-assign*/
+}
 
 export default function BreedDetail() {
   const dispatch = useDispatch();
   const breeds = useSelector((state) => state.allBreeds);
   const { id } = useParams();
 
-  let detail = breeds.filter((breed) => breed.id === id || breed.id === Number(id));
+  let detail = breeds.filter(
+    (breed) => breed.id === id || breed.id === Number(id),
+  );
 
   useEffect(() => {
     dispatch(getBreeds());
-    return () => detail = {};
+    return () => (detail = {});
   }, []);
 
   if (detail[0].id.length > 3) {
@@ -30,21 +34,27 @@ export default function BreedDetail() {
   return (
     <main>
       <section>
-        <Link to="/home" className={Style.link}>Back Home</Link>
+        <Link to="/home" className={Style.link}>
+          Back Home
+        </Link>
       </section>
       <section className={Style.container}>
-        <img className={Style.image} src={detail[0].image.url} alt="Not found" />
+        <img
+          className={Style.image}
+          src={detail[0].image.url}
+          alt="Not found"
+        />
         <section>
-          <h3>
-            {detail[0].name}
-          </h3>
+          <h3>{detail[0].name}</h3>
         </section>
         <h4>Height (cm): {detail[0].height.metric}</h4>
         <h4>Weight (kg): {detail[0].weight.metric}</h4>
-        <h4>
-          Life span: {detail[0].life_span}
-        </h4>
-        { detail[0].temperament ? <h4>Temperaments: {detail[0].temperament}</h4> : <h4>Temperaments: None</h4> }
+        <h4>Life span: {detail[0].life_span}</h4>
+        {detail[0].temperament ? (
+          <h4>Temperaments: {detail[0].temperament}</h4>
+        ) : (
+          <h4>Temperaments: None</h4>
+        )}
       </section>
     </main>
   );
