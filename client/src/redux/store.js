2dca40666157f3b5f39a33cdad55ed7ca5002262
Nacho Hotz <juanhotz611@gmail.com>
2021-08-24 20:51:35 -0300

    fix linter in store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,6 +3,9 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducer/index';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
 
 export default store;
