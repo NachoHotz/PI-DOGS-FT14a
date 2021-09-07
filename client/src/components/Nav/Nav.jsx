0be26a85f793e9376a012dcce48b59011ac9b27d
Nacho Hotz <juanhotz611@gmail.com>
2021-09-07 00:32:54 -0300

    finish navbar style

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Filters from '../Filters/Filter';
-import Order from '../Order/Order';
 import Searchbar from '../Searchbar/Searchbar';
 import Style from './Nav.module.css';
 
 export default function Nav() {
   return (
-    <header>
+    <header className={Style.header}>
       <nav className={Style.navBar_container}>
         <Searchbar />
-        <Filters />
-        <Order />
         <Link to="/home/creation" className={Style.create}>
-          Create Breed
+          Create breed
         </Link>
       </nav>
     </header>
