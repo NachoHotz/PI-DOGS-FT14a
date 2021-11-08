1afe2259d04ffa9e270c193bed40c7a5ce2f903a
Nacho Hotz <juanhotz611@gmail.com>
2021-11-08 01:13:03 -0300

    fix components imports in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Landing from '../components/Landing/Landing';
-import Home from '../components/Home/Home';
-import BreedDetail from '../components/BreedDetail/BreedDetail';
-import BreedCreation from '../components/BreedCreation/BreedCreation';
+import Landing from './components/Landing/Landing';
+import Home from './components/Home/Home';
+import BreedDetail from './components/BreedDetail/BreedDetail';
+import BreedCreation from './components/BreedCreation/BreedCreation';
 import './App.css';
 
 function App() {
