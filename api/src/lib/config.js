2995845442e5f26823f2acc54d04b39632d57be8
Nacho Hotz <juanhotz611@gmail.com>
2021-11-08 00:28:56 -0300

    modify config.js keys order

--- api/src/lib/config.js
+++ api/src/lib/config.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 
 const config = {
   API_KEY: process.env.API_KEY,
+  API_PORT: process.env.API_PORT,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_HOST: process.env.DB_HOST,
   DB_NAME: process.env.DB_NAME,
   DB_PORT: process.env.DB_PORT,
-  API_PORT: process.env.API_PORT,
 }
 
 module.exports = config;
