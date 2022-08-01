82a7ea5277477de9c8759a63520cd4587ed9af58
Nacho Hotz <juanhotz611@gmail.com>
2022-08-01 20:48:17 -0300

    FIX: remove db comments. Remove cookie-parser. Config cors origin

--- api/src/config/cors.js
+++ api/src/config/cors.js
@@ -1,3 +1,14 @@
+import dotenv from 'dotenv';
+import { envConfig } from './env';
+
+dotenv.config();
+
+const { NODE_ENV } = envConfig;
+
 export const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: `${
+    NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : 'https://cors-anywhere.herokuapp.com'
+  }`,
 };
--- api/src/server.js
+++ api/src/server.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
 import cors from 'cors';
 import v1Routes from './v1/routes/index.routes.js';
@@ -11,7 +10,6 @@ const server = express();
 
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
-server.use(cookieParser());
 server.use(helmet({ hidePoweredBy: true }));
 server.use(cors(corsOptions));
 
