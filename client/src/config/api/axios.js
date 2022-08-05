f2d02661ffdf6e896a8ef670612a41ac362d5a56
Nacho Hotz <juanhotz611@gmail.com>
2022-08-04 21:15:58 -0300

    TASK: API base url. Envs. Example env

--- client/src/config/api/axios.js
+++ client/src/config/api/axios.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
+import { envConfig } from '../env';
+
+const { NODE_ENV, API_CURRENT_VERSION, API_DEV_URL, API_PROD_URL } = envConfig;
+
+const BASE_URL =
+  NODE_ENV === 'development'
+    ? `${API_DEV_URL}/${API_CURRENT_VERSION}`
+    : `${API_PROD_URL}/${API_CURRENT_VERSION}`;
 
 const ClientAxios = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
---
+++ client/src/config/env.js
@@ -0,0 +1,6 @@
+export const envConfig = {
+  API_CURRENT_VERSION: process.env.REACT_APP_API_VERSION,
+  API_DEV_URL: process.env.REACT_APP_DEV_API_URL,
+  API_PROD_URL: process.env.REACT_APP_PROD_API_URL,
+  NODE_ENV: process.env.NODE_ENV,
+};
