e22cead348333f9a7194ff7e7186ca929c07d4c8
nachohotz <juanhotz611@gmail.com>
2022-09-19 20:55:52 -0300

    FIX: db connection

--- api/src/db/index.js
+++ api/src/db/index.js
@@ -1,10 +1,10 @@
 import { Sequelize } from 'sequelize';
 import { envConfig } from '../config/env.js';
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = envConfig;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = envConfig;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   {
     logging: false,
     native: false,
