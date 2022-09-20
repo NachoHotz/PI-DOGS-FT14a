82e5f6116bbe248183a83c7eb2264aebf0dfee0b
nachohotz <juanhotz611@gmail.com>
2022-09-19 22:00:05 -0300

    TASK: check CORS

--- api/src/config/cors.js
+++ api/src/config/cors.js
@@ -5,5 +5,5 @@ const { NODE_ENV, CLIENT_DEV_URL, CLIENT_PROD_URL } = envConfig;
 console.log(envConfig)
 
 export const corsOptions = {
-  origin: NODE_ENV === 'development' ? CLIENT_DEV_URL : CLIENT_PROD_URL,
+  origin: NODE_ENV === 'development' ? CLIENT_DEV_URL : 'https://dogspedia.vercel.app/',
 };
